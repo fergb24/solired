c380897cc5031ac03d0eae640d434efef56c43cd
fergb24 <fernandogbernabe@gmail.com>
2025-03-06 12:51:27 +0100

    Creación y puesta a punto de la landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent }, // Página de bienvenida
+  { path: '**', redirectTo: '' } // Redirección para rutas desconocidas
+];
